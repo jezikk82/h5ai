3addd67d31908d305a72d00b77aa18b150db3c53
Clément Péron <peron.clem@gmail.com>
2014-06-28 15:21:58 +0200

    Add IP Filtering

--- src/_h5ai/server/php/index.php
+++ src/_h5ai/server/php/index.php
@@ -23,13 +23,17 @@ normalized_require_once("class-image");
 setup();
 $app = new App();
 $options = $app->get_options();
-if ($options["security"]["enabled"] && (!isset($_SERVER['PHP_AUTH_USER']) || !isset($_SERVER['PHP_AUTH_PW'])
+if ($options["security"]["enabled"] &&
+	( !isset($_SERVER['PHP_AUTH_USER'])
+	|| !isset($_SERVER['PHP_AUTH_PW'])
 	|| ($_SERVER['PHP_AUTH_USER'] !== $options["security"]["login"])
-	|| (md5($_SERVER['PHP_AUTH_PW']) !== $options["security"]["pass"]) )) {
+	|| (md5($_SERVER['PHP_AUTH_PW']) !== $options["security"]["password"])
+	|| !(empty($options["security"]["allowedips"]) || in_array($_SERVER['REMOTE_ADDR'], $options["security"]["allowedips"]))
+        )
+) {
 
 	header('WWW-Authenticate: Basic realm='.$options["security"]["message"]);
 	header('HTTP/1.0 401 Unauthorized');
-	echo 'Acces non autorisé';
 	exit;
 }
 else if (has_request_param("action")) {
